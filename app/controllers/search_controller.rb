7ab39e18691bbb6424f241e1b6eb114c2c86179f
Antonio Jackson <antoniojackson@gmail.com>
2020-07-02 11:12:48 -0600

    Dream driving the search controller to create Member objects

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,7 @@
 class SearchController < ApplicationController
 
   def index
+    state = params[:state]
     # This is the faraday connection that has the base url for the api
     conn = Faraday.new("https://api.propublica.org") do |req|
       # The key for the header must match the Propublica API docs in order to work
@@ -8,12 +9,14 @@ class SearchController < ApplicationController
     end
 
     # The response is where the GET request is being made to the api and takes an argument of the endpoints specific path.
-    response = conn.get("/congress/v1/members/house/#{params[:state]}/current.json")
+    response = conn.get("/congress/v1/members/house/#{state}/current.json")
 
     # Using `JSON.parse` it is now a ruby hash which is easier to work with
     parsed_response = JSON.parse(response.body, symbolize_names: true)
 
     # @members is an array of member hashes
-    @members = parsed_response[:results]
+    @members = parsed_response[:results].map do |member_data|
+      Member.new(member_data)
+    end
   end
 end
