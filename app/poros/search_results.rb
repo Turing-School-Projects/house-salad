64475ae9a3b1a58225bc999b140141d73a6aa8ac
Antonio Jackson <antoniojackson@gmail.com>
2020-07-02 11:51:23 -0600

    Refactoring PropublicaService to separate the connection to the API from the specific get request for members_of_house

--- app/poros/search_results.rb
+++ app/poros/search_results.rb
@@ -1,7 +1,6 @@
 class SearchResults
   def members(state)
     parsed_response = PropublicaService.new.members_of_house(state)
-    # @members is an array of member hashes
     @members = parsed_response[:results].map do |member_data|
       Member.new(member_data)
     end
--- app/services/propublica_service.rb
+++ app/services/propublica_service.rb
@@ -1,10 +1,13 @@
 class PropublicaService
   def members_of_house(state)
-    conn = Faraday.new("https://api.propublica.org") do |req|
-      req.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
-    end
-
     response = conn.get("/congress/v1/members/house/#{state}/current.json")
-    parsed_response = JSON.parse(response.body, symbolize_names: true)
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def conn
+    Faraday.new("https://api.propublica.org") do |req|
+      req.headers["X-API-Key"] = ENV["PROPUBLICA_API_KEY"]
+      req.adapter Faraday.default_adapter
+    end
   end
 end
